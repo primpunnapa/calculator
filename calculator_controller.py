36adaf03eae3633afded519fbff8bb81076fe1a5
Punnapa Pramotekul <punnapa.pr@ku.th>
2024-02-18 17:59:07 +0700

    implent code in calculator_controller.py

--- calculator_controller.py
+++ calculator_controller.py
@@ -1,9 +1,29 @@
+"""For controller of calculator"""
+
+
 class CalculatorController:
+    """
+    Methods:
+        __init__(self, model, ui): Initializes the CalculatorController.
+        handle_click(self, key): Handles button clicks, updating the model and UI accordingly.
+    """
+
     def __init__(self, model, ui):
+        """
+        Initializes the CalculatorController.
+
+        :param model: CalculatorModel
+        :param ui: Calculator_UI
+        """
         self.model = model
         self.ui = ui
 
     def handle_click(self, key):
+        """
+        Handles button clicks, updating the model and UI accordingly.
+
+        :param key: The key representing the button clicked.
+        """
         self.model.update_expression(key)
         self.ui.display.config(text=self.model.expression)
 
