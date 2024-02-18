94750e6db61123710820476c919efe758b4a87ff
Punnapa Pramotekul <punnapa.pr@ku.th>
2024-02-18 17:46:10 +0700

    implent code 2

--- calculator_model.py
+++ calculator_model.py
@@ -21,34 +21,18 @@ class CalculatorModel:
         except Exception as e:
             return "Error"
 
-    def calculate_function(self, func, x=None):
-        try:
-            if func == "exp":
-                return str(exp(x))
-            elif func == "ln":
-                return str(log(x))
-            elif func == "log10":
-                return str(log10(x))
-            elif func == "log2":
-                return str(log2(x))
-            elif func == "sqrt":
-                return str(sqrt(x))
-            else:
-                return "Error: Unknown function"
-        except Exception as e:
-            return "Error"
 
     def handle_del(self):
-            if self.expression.endswith('sqrt('):
-                self.expression = self.expression[:-5]
-            elif self.expression.endswith('log2('):
-                self.expression = self.expression[:-5]
-            elif self.expression.endswith('log10('):
-                self.expression = self.expression[:-6]
-            elif self.expression.endswith('**'):
-                self.expression = self.expression[:-2]
-            elif len(self.expression) > 0:
-                self.expression = self.expression[:-1]
+        if self.expression.endswith('sqrt('):
+            self.expression = self.expression[:-5]
+        elif self.expression.endswith('log2('):
+            self.expression = self.expression[:-5]
+        elif self.expression.endswith('log10('):
+            self.expression = self.expression[:-6]
+        elif self.expression.endswith('**'):
+            self.expression = self.expression[:-2]
+        elif len(self.expression) > 0:
+            self.expression = self.expression[:-1]
 
     def handle_clear(self):
         self.expression = ""
--- calculator_ui.py
+++ calculator_ui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from keypad import Keypad
 from calculator_model import CalculatorModel
+from math import *
 
 
 class Calculator_UI(tk.Tk):
@@ -16,13 +17,13 @@ class Calculator_UI(tk.Tk):
         self.display.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
         self.display.configure(background='#FFA07A', font=('Charter', 40), fg='#33A1C9')
 
-        self.function_combobox = ttk.Combobox(self, values=['exp', 'ln', 'log10', 'log2', 'sqrt'])
-        self.function_combobox.pack(side=tk.TOP, padx=2, pady=2)
-        self.function_combobox.configure(font=('Charter', 16))
-        self.function_combobox.bind('<<ComboboxSelected>>', lambda event: self.handle_click(event))
+        # self.function_combobox = ttk.Combobox(self, values=['exp', 'ln', 'log10', 'log2', 'sqrt'])
+        # self.function_combobox.pack(side=tk.TOP, padx=2, pady=2)
+        # self.function_combobox.configure(font=('Charter', 16))
+        # self.function_combobox.bind('<<ComboboxSelected>>', lambda event: self.handle_click(event))
 
-        keypad = Keypad(self,['7','8','9','4','5','6','1','2','3','CLR','0','.'], columns=3)
-        operation = Keypad(self,['*', '/', '+', '-', '^', 'mod', '(', ')', '=', 'DEL'], columns=2)
+        keypad = Keypad(self,['7','8','9','4','5','6','1','2','3',' ','0','.'], columns=3)
+        operation = Keypad(self,['*', '/', '+', '-', '^', 'mod', '(', ')', '=', 'exp', 'ln', 'log10', 'log2', 'sqrt','DEL', 'CLR'], columns=2)
 
         keypad.bind('<Button>', self.handle_click)
         keypad.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
@@ -40,19 +41,8 @@ class Calculator_UI(tk.Tk):
     def handle_click(self, event):
         key = event.widget['text']
         if key == "=":
-            selected_function = self.function_combobox.get()
-            if selected_function:
-                # Check if the expression is just a number or a function with an argument
-                if '(' in self.model.expression:
-                    # If there's an argument, evaluate the expression to get the value of 'x'
-                    x = eval(self.model.expression)
-                    result = self.model.calculate_function(selected_function, x)
-                else:
-                    # If no argument, directly call calculate_function without 'x'
-                    result = self.model.calculate_function(selected_function)
-                print(result + 'test')
-            else:
-                result = self.model.evaluate_expression()
+            result = self.model.evaluate_expression()
+            print(result + 'test')
 
             if result != "Error":
                 self.display.config(fg='#33A1C9')
@@ -85,22 +75,20 @@ class Calculator_UI(tk.Tk):
             self.model.handle_del()
             self.display.config(text=self.model.expression)
 
-        else:
-            # Append the clicked key or selected function to the expression
-            if self.function_combobox.get() != "":
-                selected_function = self.function_combobox.get()
-                if self.model.expression:
-                    # Check if the last character in the expression is a number
-                    if self.model.expression[-1].isdigit():
-                        self.model.expression = selected_function + '(' + self.model.expression + ')'
-                    else:
-                        self.model.expression = selected_function + '('
-                    self.display.config(text=self.model.expression)
-                    self.model.handle_clear()
-                    self.function_combobox.set("")
+        elif key in {'sqrt', 'exp', 'log2', 'log10', 'ln'}:
+            # Handle special functions
+            if self.model.expression and self.model.expression[-1].isdigit():
+                # If the last character is a digit, append the special function with a left parenthesis
+                self.model.expression = key + '(' + self.model.expression + ')'
             else:
-                self.model.expression += key
-                self.display.config(text=self.model.expression)
+                # If the last character is an operator, append the special function
+                self.model.expression = self.model.expression + key + '('
+            self.display.config(text=self.model.expression)
+
+        else:
+            self.model.expression += key
+            self.display.config(text=self.model.expression)
+
 
     def run(self):
         self.mainloop()
